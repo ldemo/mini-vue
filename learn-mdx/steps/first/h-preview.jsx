f4b5cbb19c21052eda141ee7856caf50ea774ac3
一路有晴天 <282868633@qq.com>
2023-03-24 23:44:04 +0800

    docs: first step

--- learn-mdx/steps/first/h-preview.jsx
+++ learn-mdx/steps/first/h-preview.jsx
@@ -7,8 +7,8 @@ export default () => {
 			<MotionConfig transition={{ duration: 0.5 }}>
 				<motion.div
 					className="py-1 text-white flex flex-col justify-center items-center"
-					initial={{ x: '100%', opacity: 0 }}
-					animate={{ x: 0, opacity: 1 }}
+					initial={{ x: '100%', opacity: 0, rotate: '30deg' }}
+					whileInView={{ x: 0, opacity: 1, rotate: 0 }}
 					transition={{ delay: 0.5 }}
 				>
 					<img src={HPreImg} />
